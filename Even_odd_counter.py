686d1b96c6d571c62352dae8fc8eeeacaaf391f9
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-02-14 01:39:21 -0800

    Code Updated

--- Even_odd_counter.py
+++ Even_odd_counter.py
@@ -1,10 +1,12 @@
+#Even-odd-counter:
 nums = list(map(int,input("enter numbers:").split()))
 even = odd = 0
 for n in nums:
-    if(n%2 == 0):
+    if(n % 2 == 0):
         even += 1
     else:
         odd += 1
 
 print("even:",even)
+
 print("odd:",odd)
