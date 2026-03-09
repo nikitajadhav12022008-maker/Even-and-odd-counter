2ebb9cf4300b6263cc64daf8f8bb95f816dd74f1
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-09 06:25:24 -0700

    Update Even_odd_counter.py

--- Even_odd_counter.py
+++ Even_odd_counter.py
@@ -1,12 +1,16 @@
-#Even-odd-counter:
+#python based Even-odd-counter:
+#list,input and split nums
 nums = list(map(int,input("enter numbers:").split()))
+#setting even and odd to 0
 even = odd = 0
+#for loop running
 for n in nums:
+    #conditions to check even and odd nums
     if(n % 2 == 0):
         even += 1
     else:
         odd += 1
-
+#finally print even and odd
 print("even:",even)
-
 print("odd:",odd)
+
